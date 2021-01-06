933e9998e620d74612cb9bba9352c355ade7fb7b
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-01-06 19:14:56 +0100

    Add method bindings in positionable::DynamicIsometricElement

--- src/positionable/dynamic_isometric_element.cpp
+++ src/positionable/dynamic_isometric_element.cpp
@@ -19,5 +19,10 @@ void DynamicIsometricElement::update_position_from_body(PhysicsBody* physicsBody
 }
 
 void DynamicIsometricElement::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("update_position_from_body"), &DynamicIsometricElement::update_position_from_body);
 
+    ClassDB::bind_method(D_METHOD("set_has_default_body"), &DynamicIsometricElement::set_has_default_body);
+    ClassDB::bind_method(D_METHOD("get_has_default_body"), &DynamicIsometricElement::get_has_default_body);
+    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "has_default_body"), "set_has_default_body", "get_has_default_body");
+    ADD_PROPERTY_DEFAULT("has_default_body", true);
 }
