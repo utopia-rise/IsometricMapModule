29d6b4a5d4b31eddd983605c0ee2515d843aee8b
Tristan Grespinet <dev003@infra.utopia-rise.com>
2021-02-05 03:24:31 +0100

    add data namespace

--- src/data/isometric_world_data.h
+++ src/data/isometric_world_data.h
@@ -4,6 +4,7 @@
 #include <core/rid.h>
 #include <modules/isometric_maps/src/isometric_world.h>
 
+namespace data {
 class IsometricWorldData : public RID_Data {
 public:
 	IsometricWorld *world;
@@ -11,6 +12,6 @@ public:
 	IsometricWorldData();
 	~IsometricWorldData() = default;
 };
-
+} // namespace data
 
 #endif //ISOMETRIC_MAPS_ISOMETRIC_WORLD_DATA_H
