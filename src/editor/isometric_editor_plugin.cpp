65d5c385a4b734921bf5fd2aa16538d5ed021a53
Tristan Grespinet <dev003@infra.utopia-rise.com>
2021-05-04 11:34:26 +0200

    add base functions

--- src/editor/isometric_editor_plugin.cpp
+++ src/editor/isometric_editor_plugin.cpp
@@ -1,8 +1,9 @@
 #include "isometric_editor_plugin.h"
+#include "../node/isometric_map.h"
 
 using namespace editor;
 
-IsometricEditorPlugin::IsometricEditorPlugin() {
+IsometricEditorPlugin::IsometricEditorPlugin(): selected_map{nullptr}, current_mode{Mode::NONE} {
 
 }
 
@@ -11,11 +12,29 @@ IsometricEditorPlugin* IsometricEditorPlugin::get_instance() {
     return instance;
 }
 
-void IsometricEditorPlugin::_bind_methods() {
-}
-
 void IsometricEditorPlugin::_notificationv(int p_notification, bool p_reversed) {
     EditorPlugin::_notificationv(p_notification, p_reversed);
 }
+
+void IsometricEditorPlugin::edit(Object* p_object) {
+    selected_map = cast_to<node::IsometricMap>(p_object);
+}
+
+bool IsometricEditorPlugin::handles(Object* p_object) const {
+    return cast_to<node::IsometricMap>(p_object);
+}
+
+void IsometricEditorPlugin::clear() {
+    selected_map = nullptr;
+}
+
+bool IsometricEditorPlugin::forward_canvas_gui_input(const Ref<InputEvent> &p_event) {
+    return false;
+}
+
+
+void IsometricEditorPlugin::_bind_methods() {
+}
+
 
 
--- src/editor/isometric_editor_plugin.h
+++ src/editor/isometric_editor_plugin.h
@@ -1,14 +1,25 @@
 #ifndef ISOMETRIC_MAPS_ISOMETRIC_EDITOR_PLUGIN_H
 #define ISOMETRIC_MAPS_ISOMETRIC_EDITOR_PLUGIN_H
 
+#include <modules/isometric_maps/src/node/isometric_map.h>
 #include "editor/editor_node.h"
 #include "editor/editor_plugin.h"
 
-namespace editor{
+namespace editor {
 
     class IsometricEditorPlugin : public EditorPlugin {
 
+        enum Mode {
+            NONE,
+            SELECT,
+            PAIN
+        };
+
         IsometricEditorPlugin();
+
+        node::IsometricMap* selected_map;
+
+        Mode current_mode;
 
     public:
         static IsometricEditorPlugin* get_instance();
@@ -19,6 +30,14 @@ namespace editor{
 
     protected:
         void _notificationv(int p_notification, bool p_reversed) override;
+
+        bool forward_canvas_gui_input(const Ref<InputEvent>& p_event) override;
+
+        void edit(Object* p_object) override;
+
+        bool handles(Object* p_object) const override;
+
+        void clear() override;
 
     public:
         static void _bind_methods();
