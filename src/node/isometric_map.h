21b5e90d03b3c04879cb374a6555d1110d7dc7cf
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-07-31 10:53:31 +0200

    clean up IsometricMap

--- src/node/isometric_map.h
+++ src/node/isometric_map.h
@@ -12,42 +12,17 @@ namespace node {
 
     private:
         bool draw_tiles;
-        containers::Grid3D grid_3d;
-        containers::Grid3D edition_grid_3d;
+        containers::Grid3D<int, -1> grid_3d;
         Ref<resource::PositionableSet> positionable_set;
 
-        Array get_flatten_positionables(const Vector3 &offset = Vector3());
-
-        void insert_map_as_flatten(Node* map, const Vector3 &offset);
-
-        void _ready();
-
-    protected:
-        void _notification(int notif);
-
     public:
+        Ref<resource::PositionableSet> get_positionable_set() const;
+
+        void set_positionable_set(const Ref<resource::PositionableSet>& set);
+
         IsometricMap();
 
         ~IsometricMap() override = default;
-
-        void add_iso_positionable(Node* isometric_node);
-
-        void remove_iso_positionable(Node* isometric_node);
-
-        Node* get_positionable_at(Vector3 pos, bool only_left_upper_corner = true);
-
-        bool is_overlapping(Node* node);
-
-        bool is_overlapping_aabb(AABB aabb);
-
-        bool are_map_elements_overlapping(Vector3 position, Node* map);
-
-        bool has(Node* isometric_node);
-
-        Array get_positionable_children() const;
-
-        Ref<resource::PositionableSet> get_positionable_set() const;
-        void set_positionable_set(const Ref<resource::PositionableSet>& set);
 
     protected:
         static void _bind_methods();
