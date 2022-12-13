9a1213900c09cd29aeb50f3f99ffdb92c610c531
Tristan Grespinet <cednaru.dev@gmail.com>
2022-12-13 19:10:06 +0100

    Add demo project (#28)
    * Add demo project

---
+++ demo/player_movement.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+
+var linear_velocity: Vector3
+
+export(float) var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+export(int) var speed: int = 1
+
+
+func _physics_process(delta):
+	linear_velocity += Vector3(0, -gravity * delta, 0)
+	var horizontal_velocity: Vector3 = Vector3()
+	var vertical_velocity: Vector3 = Vector3(0, linear_velocity.y, 0)
+
+	if Input.is_action_pressed("player_forward"):
+		horizontal_velocity += Vector3(-1, 0, -1).normalized() * speed
+	if Input.is_action_pressed("player_backward"):
+		horizontal_velocity += Vector3(1, 0, 1).normalized() * speed
+	if Input.is_action_pressed("player_left"):
+		horizontal_velocity += Vector3(-1, 0, 1).normalized() * speed
+	if Input.is_action_pressed("player_right"):
+		horizontal_velocity += Vector3(1, 0, -1).normalized() * speed
+
+
+	linear_velocity = horizontal_velocity + vertical_velocity
+	linear_velocity = move_and_slide(linear_velocity, Vector3(0, 1, 0))
