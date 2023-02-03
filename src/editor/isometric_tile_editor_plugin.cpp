9549ea8ab0d385734b80cff27e9279d3ab5a6211
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2023-02-03 17:46:27 +0100

    feat: Allow to change outline color using color picker

--- src/editor/isometric_tile_editor_plugin.cpp
+++ src/editor/isometric_tile_editor_plugin.cpp
@@ -2,13 +2,14 @@
 
 #include "isometric_tile_editor_plugin.h"
 
-#include "node/isometric_map.h"
-#include "node/isometric_positionable.h"
+#include <node/isometric_map.h>
 #include "outline_drawer.h"
 
 #include <editor/editor_node.h>
 
 using namespace editor;
+
+static constexpr const char* OUTLINE_COLOR_TITLE{"Outline color:"};
 
 IsometricTileEditorPlugin* IsometricTileEditorPlugin::get_instance() {
     static IsometricTileEditorPlugin* instance {nullptr};
@@ -22,11 +23,22 @@ bool IsometricTileEditorPlugin::handles(Object* p_object) const {
     return cast_to<node::IsometricPositionable>(p_object) && !cast_to<node::IsometricMap>(p_object);
 }
 
+void IsometricTileEditorPlugin::_notification(int p_notification) {
+    if (p_notification != NOTIFICATION_READY) {
+        return;
+    }
+
+    add_control_to_container(CustomControlContainer::CONTAINER_CANVAS_EDITOR_MENU, toolbar);
+
+    toolbar->add_child(memnew(Label(OUTLINE_COLOR_TITLE)));
+    toolbar->add_child(color_picker_button);
+}
+
 void IsometricTileEditorPlugin::edit(Object* p_object) {
     selected_positionable = cast_to<node::IsometricPositionable>(p_object);
     OutlineData& outline_data {selected_positionable->get_outline_data()};
     outline_data.line_size = 1.0;
-    outline_data.color = {0, 0, 0, 1};
+    _on_color_picker_change(color_picker_button->get_pick_color());
     OutlineDrawer::draw_outline(selected_positionable);
     OutlineDrawer::set_outline_visible(selected_positionable, true);
 
@@ -35,14 +47,30 @@ void IsometricTileEditorPlugin::edit(Object* p_object) {
     }
 }
 
+void IsometricTileEditorPlugin::make_visible(bool p_visible) {
+    toolbar->set_visible(p_visible);
+}
+
 void IsometricTileEditorPlugin::_on_size_changed() {
     OutlineDrawer::draw_outline(selected_positionable);
 }
 
-IsometricTileEditorPlugin::IsometricTileEditorPlugin() : selected_positionable{nullptr} {}
+void IsometricTileEditorPlugin::_on_color_picker_change(const Color& p_color) {
+    selected_positionable->get_outline_data().color = p_color;
+    OutlineDrawer::draw_outline(selected_positionable);
+}
+
+IsometricTileEditorPlugin::IsometricTileEditorPlugin() :
+  selected_positionable{nullptr},
+  toolbar{ memnew(HBoxContainer) },
+  color_picker_button{ memnew(ColorPickerButton) } {
+    color_picker_button->set_text(OUTLINE_COLOR_TITLE);
+    color_picker_button->connect("color_changed", this, "_on_color_picker_change");
+}
 
 void IsometricTileEditorPlugin::_bind_methods() {
     ClassDB::bind_method("_on_size_changed", &IsometricTileEditorPlugin::_on_size_changed);
+    ClassDB::bind_method(D_METHOD("_on_color_picker_change", "p_color"), &IsometricTileEditorPlugin::_on_color_picker_change);
 }
 
 #endif
